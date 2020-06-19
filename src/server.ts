1cc1ea10f7203970e741d69897e5d474833e22b8
Sinthu <kumarasamy.s@husky.neu.edu>
2020-06-18 21:31:11 -0400

    Added typescript changes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express,{Request,Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,7 +30,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  app.get("/filteredImage",async(req,res)=>{
+  app.get("/filteredImage",async(req: Request, res: Response)=>{
     const img_url:string = req.query.image_url;
     if(!img_url){
       return res.status(400).send("Not an valid image")
